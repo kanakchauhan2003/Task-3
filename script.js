010c7a42326c2666375a6548fa9190768a42884d
Kanak Chauhan <146485707+kanakchauhan2003@users.noreply.github.com>
2024-09-22 23:24:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+function insertValue(value){
+    result.value += value;
+}
+
+
+function clearResult(){
+    result.value = '';
+}
+
+
+
+function deleteResult(){
+result.value = result.value.slice(0, -1);
+}
+
+
+function calculate(){
+    try{
+        result.value = eval(result.value);
+
+    } catch (error){
+        result.value = 'Error';
+    }
+}
